ba6795fb10381f7df62097f3c11f09275ec5d35e
AlkVo <halfsteamedbun@gmail.com>
2020-06-06 19:25:02 +0800

    修改 登录 、未登录时的 navbar 状态

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
+import authContext from '../../context/auth/authContext';
+
 const Navbar = ({ title, icon }) => {
+  const AuthContext = useContext(authContext);
+  const { isAuthenticated, user, logout } = AuthContext;
+
+  const onLogout = () => {
+    logout();
+  };
+  const userLink = (
+    <Fragment>
+      <li>Hello , {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href='#!'>
+          <i className='fas fa-sign-out-alt'></i>
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLink = (
+    <Fragment>
+      <li>
+        <Link to='/register'>注册</Link>
+      </li>
+      <li>
+        <Link to='/login'>登录</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className='navbar bg-primary'>
       <h1>
         <i className={icon} /> {title}
       </h1>
-      <ul>
-        <li>
-          <Link to='/'>主页</Link>
-        </li>
-        <li>
-          <Link to='/about'>关于</Link>
-        </li>
-        <li>
-          <Link to='/rigster'>注册</Link>
-        </li>
-        <li>
-          <Link to='/login'>登录</Link>
-        </li>
-      </ul>
+      <ul>{isAuthenticated ? userLink : guestLink}</ul>
     </div>
   );
 };
