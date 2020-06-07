35db5c15b67c9e6631e14e3780721bb8d592f799
AlkVo <halfsteamedbun@gmail.com>
2020-06-07 18:06:30 +0800

    将 connect db 改为 async

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-//producttion 会从 process 里面读
-//开发环境下是 5000
-const PORT = process.env.PORT || 5000;
-
 //定义 routes
 app.use('/api/users', require('./routers/users'));
 app.use('/api/auth', require('./routers/auth'));
@@ -26,6 +22,10 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
+
+//producttion 会从 process 里面读
+//开发环境下是 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
