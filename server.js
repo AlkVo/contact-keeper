556aaa16123f4aa5ea5c5ff7a393aeb03731346c
AlkVo <halfsteamedbun@gmail.com>
2020-06-07 17:33:16 +0800

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -16,6 +17,15 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/users', require('./routers/users'));
 app.use('/api/auth', require('./routers/auth'));
 app.use('/api/contacts', require('./routers/contacts'));
+
+//在 producttion 环境的 static assets
+if (process.env.NODE_ENV === 'production') {
+  //设置 静态 文件夹
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'bulid', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
